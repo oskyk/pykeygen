c66202a778bcd44e531e1c508ae40ba5e53031b7
Oskar Hladky <oskar.hladky@bitpanda.com>
2018-03-06 15:42:58 +0100

    pykeygen

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='pykeygen',
+      version='0.0.1',
+      packages=find_packages(),
+      install_requires=['bip32utils', 'chainside-btcpy'],
+      dependency_links=['git+https://github.com/oskyk/btcpy.git@altcoin-constants-filled'],
+      description='Tool to generate keys and addresses',
+      author='Oskar Hladky',
+      author_email='oskyks1@gmail.com',
+      url='https://github.com/oskyk/pykeygen',
+      python_requires='>=2.7',
+      scripts=['pykeygen'],
+      keywords=['keygen', 'mnemonic', 'segwit', 'dash', 'litecoin', 'bitcoin']
+)
