3b8b90ddeedba7a09dd0cd8fddbcf7b223a5ebd8
Oskar Hladky <oskar.hladky@bitpanda.com>
2018-03-07 13:10:43 +0100

    change chainside-btcpy to chainside-btcpy-multi

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='pykeygen',
-      version='0.0.1',
+      version='0.0.2',
       packages=find_packages(),
-      install_requires=['bip32utils', 'chainside-btcpy'],
-      dependency_links=['git+https://github.com/oskyk/btcpy.git@altcoin-constants-filled'],
+      install_requires=['bip32utils', 'chainside-btcpy-multi'],
       description='Tool to generate keys and addresses',
       author='Oskar Hladky',
       author_email='oskyks1@gmail.com',
